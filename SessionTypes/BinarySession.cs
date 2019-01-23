04c8e0c9034c47c57fca615d43d03b1cf8963e9f
curegit <37978051+curegit@users.noreply.github.com>
2019-01-23 23:24:40 +0900

    Initial Commit

---
+++ SessionTypes/BinarySession.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace SessionTypes.Binary
+{
+	public static class BinarySession<P> where P : SessionType
+	{
+		private static (Client<P> client, Server<P> server) New()
+		{
+			return (NewClient(), NewServer());
+		}
+
+		private static Client<P> NewClient()
+		{
+			return new Client<P>();
+		}
+
+		private static Server<P> NewServer()
+		{
+			return new Server<P>();
+		}
+
+		public static Client<P> Fork(Action<Server<P>> threadFunction)
+		{
+			var (client, server) = New();
+			var threadStart = new ThreadStart(() => threadFunction(server));
+			var serverThread = new Thread(threadStart);
+			serverThread.Start();
+			return client;
+		}
+	}
+
+	public static class BinarySession
+	{
+		public static Server<S> Send<T, S>(this Server<Respond<T, S>> respond, T value) where S : SessionType
+		{
+			return new Server<S>();
+		}
+
+		public static Client<S> Send<T, S>(this Client<Request<T, S>> request, T value) where S : SessionType
+		{
+			return new Client<S>();
+		}
+
+		public static Server<S> Receive<T, S>(this Server<Request<T, S>> request) where S : SessionType
+		{
+			return new Server<S>();
+		}
+
+		public static Client<S> Receive<T, S>(this Client<Respond<T, S>> respond) where S : SessionType
+		{
+			return new Client<S>();
+		}
+	}
+
+	public sealed class Server<S> where S : SessionType { }
+
+	public sealed class Client<S> where S : SessionType { }
+
+	public abstract class SessionType { }
+
+	public sealed class Request<T, S> : SessionType where S : SessionType { }
+
+	public sealed class Respond<T, S> : SessionType where S : SessionType { }
+
+	/*
+	public sealed class Choose<SL, SR> : SessionType where SL : SessionType where SR : SessionType { }
+
+	public sealed class Offer<SL, SR> : SessionType where SL : SessionType where SR : SessionType { }
+	*/
+
+	public sealed class Close : SessionType { }
+}
---
+++ SessionTypesConsoleDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SessionTypes.Binary;
+
+namespace SessionTypesConsoleDemo
+{
+	// エイリアスを貼ることもできる
+	//using MyProtocol = Request<int, Respond<string, Close>>;
+
+	public class Program
+	{
+		private static async Task Main(string[] args)
+		{
+			Console.WriteLine("Hello Session-Types!");
+
+			var client = BinarySession<Request<int, Respond<string, Close>>>.Fork(async server =>
+			{
+				Console.WriteLine("Server Start");
+
+				// 拒否できる
+				// var server1 = await server.Send("ng");
+
+				var server1 = await Task.Run(() => server.Receive());
+
+				// 関数の形でも書ける
+				// var server1 = await BinarySession.Receive(server);
+
+				var server2 = server1.Send("ok");
+
+				Console.WriteLine("Server End");
+			});
+
+			Console.WriteLine("Client Start");
+
+			// クライアント側の処理
+			Thread.Sleep(1000);
+
+			// これは拒否
+			// var client1 = await client.Receive();
+
+			var client1 = client.Send(2);
+
+			// これも拒否
+			// var client2 = client1.Send(10); 
+
+			var client2 = await Task.Run(() => client1.Receive());
+
+			Console.WriteLine("Client End");
+		}
+	}
+}
