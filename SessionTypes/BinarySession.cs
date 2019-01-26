d610044e1d40b33f419470fbd174333d18e271e6
curegit <37978051+curegit@users.noreply.github.com>
2019-01-26 22:11:53 +0900

    分岐を仮実装

--- SessionTypes/BinarySession.cs
+++ SessionTypes/BinarySession.cs
@@ -51,6 +51,36 @@ namespace SessionTypes.Binary
 		{
 			return new Client<S>();
 		}
+
+		public static Server<SL> ChooseLeft<SL, SR>(this Server<RespondChoice<SL, SR>> respondChoice) where SL : SessionType where SR : SessionType
+		{
+			return new Server<SL>();
+		}
+
+		public static Server<SR> ChooseRight<SL, SR>(this Server<RespondChoice<SL, SR>> respondChoice) where SL : SessionType where SR : SessionType
+		{
+			return new Server<SR>();
+		}
+
+		public static Client<SL> ChooseLeft<SL, SR>(this Client<RequestChoice<SL, SR>> requestChoice) where SL : SessionType where SR : SessionType
+		{
+			return new Client<SL>();
+		}
+
+		public static Client<SR> ChooseRight<SL, SR>(this Client<RequestChoice<SL, SR>> requestChoice) where SL : SessionType where SR : SessionType
+		{
+			return new Client<SR>();
+		}
+
+		public static void Follow<SL, SR>(this Server<RequestChoice<SL, SR>> requestChoice, Action<Server<SL>> leftAction, Action<Server<SR>> rightAction) where SL : SessionType where SR : SessionType
+		{
+
+		}
+
+		public static void Follow<SL, SR>(this Client<RespondChoice<SL, SR>> respondChoice, Action<Client<SL>> leftAction, Action<Client<SR>> rightAction) where SL : SessionType where SR : SessionType
+		{
+
+		}
 	}
 
 	public sealed class Server<S> where S : SessionType { }
@@ -63,11 +93,9 @@ namespace SessionTypes.Binary
 
 	public sealed class Respond<T, S> : SessionType where S : SessionType { }
 
-	/*
-	public sealed class Choose<SL, SR> : SessionType where SL : SessionType where SR : SessionType { }
+	public sealed class RequestChoice<SL, SR> : SessionType where SL : SessionType where SR : SessionType { }
 
-	public sealed class Offer<SL, SR> : SessionType where SL : SessionType where SR : SessionType { }
-	*/
+	public sealed class RespondChoice<SL, SR> : SessionType where SL : SessionType where SR : SessionType { }
 
 	public sealed class Close : SessionType { }
 }
--- SessionTypesConsoleDemo/Program.cs
+++ SessionTypesConsoleDemo/Program.cs
@@ -47,6 +47,44 @@ namespace SessionTypesConsoleDemo
 			var client2 = await Task.Run(() => client1.Receive());
 
 			Console.WriteLine("Client End");
+
+
+
+
+			// ---- 分岐　(RS flip-flop)----
+			var client10 = BinarySession<Request<bool, Request<bool, Request<bool, RespondChoice<Respond<bool, Close>, Respond<string, Close>>>>>>.Fork(async server =>
+			{
+				// サーバーサイド
+				var s1 = server.Receive();
+				bool a = true; // 実際にはここに通信で得た値を入れる
+				var s2 = s1.Receive();
+				bool b = false; // 実際にはここに通信で得た値を入れる
+				var s3 = s2.Receive();
+				bool c = true; // 実際にはここに通信で得た値を入れる
+				if (b && c)
+				{
+					s3.ChooseRight().Send("Not allowed");
+				}
+				else
+				{
+					s3.ChooseLeft().Send((b || c) ? b : a);
+				}
+			});
+
+			// クライアントサイド
+			var client11 = client10.Send(true).Send(false).Send(true);
+			client11.Follow(
+			async left =>
+			{
+				// Receive<bool, Close>
+				var b = left.Receive();
+			},
+			async right =>
+			{
+				// Receive<string, Close>
+				var s = right.Receive();
+			}
+			);
 		}
 	}
 }
