02665031fedf3f3d633f6150fc8977e6ae18ffdd
curegit <37978051+curegit@users.noreply.github.com>
2019-04-21 22:51:16 +0900

    線形性の例外クラスを用意

---
+++ SessionTypes/LinearityViolationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SessionTypes
+{
+	[Serializable]
+	public sealed class LinearityViolationException : InvalidOperationException
+	{
+		public LinearityViolationException() : base() { }
+
+		public LinearityViolationException(string message) : base(message) { }
+
+		public LinearityViolationException(string message, Exception inner) : base(message, inner) { }
+
+		private LinearityViolationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
