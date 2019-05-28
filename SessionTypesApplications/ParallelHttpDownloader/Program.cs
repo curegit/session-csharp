42843acd68894629c76f7614443104b27d20e708
curegit <37978051+curegit@users.noreply.github.com>
2019-05-29 00:24:22 +0900

    壁紙テスト

--- SessionTypesApplications/ParallelHttpDownloader/Program.cs
+++ SessionTypesApplications/ParallelHttpDownloader/Program.cs
@@ -15,13 +15,43 @@ namespace ParallelHttpDownloader
 		private static void Main(string[] args)
 		{
 			Console.WriteLine("Parallel HTTP Downloader");
-			var n = 2;
+			var n = Environment.ProcessorCount;
+			Console.WriteLine($"{n} Threads");
 			args = new string[]
 			{
 				"http://www.toei-anim.co.jp/tv/precure/images/special/wallpaper/01_sp1080_1920.jpg",
 				"http://www.toei-anim.co.jp/tv/precure/images/special/wallpaper/01_pc1920_1080.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/04/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/06/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/12/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/01_sdwu/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/05dmck/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/09/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/10/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/11/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/maho/img/enjoyment/wallpaper/12/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/01/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/02/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/03/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/04/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/05/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/06/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/07/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/08/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/09/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/10/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/11/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/kirakira/img/enjoyment/wallpaper/12/1920_1080_2.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure091_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure081_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure071_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure061_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure051_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure041_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure031_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure021_1920.jpg",
+				"https://www.asahi.co.jp/precure/princess/img/enjoyment/wallpaper/precure12_1920.jpg",
 			};
-			
 
 			var ids = Enumerable.Range(1, n).ToArray();
 
@@ -33,8 +63,18 @@ namespace ParallelHttpDownloader
 				{
 					var flag = false;
 					s.Follow(
-						left => { s = left.Receive(out var url).Send(Download(http, url)).Zero(); },
-						right => { flag = true; right.Close(); }
+						left => {
+							var s1 = left.Receive(out var url);
+							Console.WriteLine($"Thread {id} Started downloading");
+							var d = Download(http, url);
+							Console.WriteLine($"Thread {id} Finished downloading");
+							var s2 = s1.Send(d);
+							s = s2.Zero();
+						},
+						right => {
+							Console.WriteLine($"Thread {id} Closed");
+							flag = true; right.Close();
+						}
 					);
 					if (flag) break;
 				}
