2bc17e11239a861985c661e644bddc13917286a7
Cure Git <37978051+curegit@users.noreply.github.com>
2019-11-07 21:15:22 +0900

    いらんフィールドを削除

--- SessionTypes/Dual.cs
+++ SessionTypes/Dual.cs
@@ -2,10 +2,6 @@ namespace SessionTypes
 {
 	public sealed class Dual<C, S> where C : ProtocolType where S : ProtocolType
 	{
-		internal C client;
-
-		internal S server;
-
 		internal Dual() { }
 	}
 }
