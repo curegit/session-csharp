b8b2e9c50c3a07e7bd576975dd32766b83b1260e
Cure Git <37978051+curegit@users.noreply.github.com>
2019-11-09 22:27:50 +0900

    たらい回し関数の例

--- SessionTypesDemos/TaraiProtocol/Program.cs
+++ SessionTypesDemos/TaraiProtocol/Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+using System;
+using System.Threading.Tasks;
+using SessionTypes;
+using SessionTypes.Threading;
 
 namespace TaraiProtocol
 {
-	class Program
+	using static ProtocolBuilder;
+
+	public class Program
 	{
-		static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var protocol = C2S(P<int>, C2S(P<int>, C2S(P<int>, S2CAdd(C2S(P<bool>, End), AtS(S2C(P<int>, End), End)))));
+
+			var client = protocol.Fork(async server =>
+			{
+				var s1 = server.Receive(out var x).Receive(out var y).Receive(out var z);
+				var s2 = s1.SendNewChannel(out var session);
+				var calc = Task.Run(() => Tarai(x, y, z));
+				var cancel = session.ReceiveAsync();
+				int i = Task.WaitAny(calc, cancel);
+				if (i == 0)
+				{
+					s2.SelectLeft().Send(calc.Result).Close();
+					(await cancel).Bind(out var b).Close();
+				}
+				else
+				{
+					s2.SelectRight().Close();
+					cancel.Result.Bind(out var b).Close();
+				}
+			});
+
+			var c1 = client.Send(16).Send(3).Send(2);
+			var c2 = c1.ReceiveNewChannel(out var ch);
+			var ret = c2.FollowAsync(left =>
+			{
+				left.Receive(out var ans).Close();
+				Console.WriteLine($"Tarai = {ans}");
+			},
+			right =>
+			{
+				right.Close();
+				Console.WriteLine("Canceled");
+			});
+			var timeout = Task.Delay(20000);
+			var task = await Task.WhenAny(ret, timeout);
+			if (task == ret)
+			{
+				ch.Send(false).Close();
+			}
+			else
+			{
+				ch.Send(true).Close();
+				await ret;
+			}
+		}
+
+		private static int Tarai(int x, int y, int z)
+		{
+			return x <= y ? y : Tarai(Tarai(x - 1, y, z), Tarai(y - 1, z, x), Tarai(z - 1, x, y));
 		}
 	}
 }
