6b41038dd7bd85ff23fada9cb610eb97262d1d02
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-12 23:57:17 +0900

    草案

--- SessionTypes/SessionTypes/Protocol.cs
+++ SessionTypes/SessionTypes/Protocol.cs
@@ -1,33 +1,40 @@
 namespace SessionTypes
 {
-	public abstract class Protocol<C, S> where C : ProtocolType where S : ProtocolType
-	{
-		// TODO: pretty print
-		public override string ToString()
-		{
-			return base.ToString();
-		}
-	}
-
 	public sealed class Protocol<C, S> where C : ProtocolType where S : ProtocolType
 	{
 		internal Protocol() { }
 
 		public Protocol<S, C> Swapped => new Protocol<S, C>();
 
-		public Protocol<C, S, F> UseSeri<F>()
+		// TODO: Pretty print
+		public override string ToString()
 		{
-			// 方検査できん
-		}
-
-		public int A()
-		{
-			return Combinator.S2C(P<>, Combinator.End);
+			return base.ToString();
 		}
 	}
 
-	public sealed class Protocol<C, S, F> : Protocol<C, S>
+	public sealed class Protocol<C, S, T> where C : ProtocolType where S : ProtocolType
 	{
+		internal Protocol() { }
 
+		public Protocol<S, C, T> Swapped => new Protocol<S, C, T>();
+
+		public StreamProtocol<C, S> ForStream(IFormatter<T> formatter)
+		{
+
+		}
+
+		// TODO: Pretty print
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+
+	public sealed class StreamProtocol<C, S, T>
+	{
+		internal StreamProtocol() { }
+
+		public IFormatter<T> Formatter { get; }
 	}
 }
