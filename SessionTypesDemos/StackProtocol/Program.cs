c5e576fc65e2190d62b408dc588bc56db458f24c
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-19 21:20:34 +0900

    Revert "stack"
    This reverts commit 936a163402fca14d9f819255d300fc60c32421c1.

--- SessionTypesDemos/StackProtocol/Program.cs
+++ SessionTypesDemos/StackProtocol/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using SessionTypes;
 using SessionTypes.Threading;
 
@@ -11,224 +10,78 @@ namespace StackProtocol
 	{
 		public static void Main(string[] args)
 		{
-			// μβ.&[Push: ?int; μα.&[Push:?int;α;β, Pop:!int;β], End:end]
-			var sub0 = AtC(C2S(P<int>, Goto1), End);
-			var sub1 = AtC(C2S(P<int>, Call1(Goto0)), S2C(P<int>, Goto0));
-			var protocol = SessionList(sub0, sub1);
+			Console.WriteLine("Hello World!");
+
+			/*
+			var p0 = AtC(C2S(P<int>, Goto1), End);
+			var p1 = AtC(C2S(P<int>, Call1(Goto0)), S2C(P<int>, Goto0));
+			var protocol = SessionList(p0, p1);
+			*/
+
+			var p0 = Call1(End);
+			var p1 = AtC(C2S(P<int>, Call1(S2C(P<int>, Goto1))), End);
+			var protocol = SessionList(p0, p1);
 
 			var client = protocol.Fork(server =>
 			{
-				var loop = false;
-				var zero = server.Enter();
-				var stack = new Stack<int>();
-				do
+				server.Enter().Call((session, thisFunc) =>
 				{
-					loop = false;
-					zero.Follow(push =>
-					{
-						var session = push.Receive(out var value);
-						stack.Push(value);
-						zero = session.Goto().Follow(push =>
-						{
-							var session = push.Receive(out var value);
-							stack.Push(value);
-							return session.Call((session, thisFunc) =>
-							{
-								return session.Follow(push =>
-								{
-									var session = push.Receive(out var value);
-									stack.Push(value);
-									return session.Call(thisFunc).Goto().Follow(push =>
-									{
-										var session = push.Receive(out var value);
-										stack.Push(value);
-										return thisFunc(session.Goto(), thisFunc);
-									},
-									end =>
-									{
-										return end;
-									});
-								},
-								pop =>
-								{
-									return pop.Send(stack.Pop()).Goto().Follow(push =>
-									{
-										var session = push.Receive(out var value);
-										stack.Push(value);
-										return thisFunc(session.Goto(), thisFunc);
-									},
-									end =>
-									{
-										return end;
-									});
-								});
-							}).Goto();
-						},
-						pop => zero = pop.Send(stack.Pop()).Goto());
-						loop = true;
-					},
-					end =>
-					{
-						end.Close();
-					});
-				} while (loop);
+					return session.Follow(
+						push => thisFunc(push.Receive(out var x).Call(thisFunc).Send(x).Goto(), thisFunc),
+						end => end
+					);
+				}).Close();
 			});
+
 
 			var counter = 0;
 			var random = new Random();
-			var zero = client.Enter();
-			while (true)
+			client.Enter().Call((session, thisFunc) =>
 			{
-				if (random.NextDouble() < 0.7)
+				if (random.NextDouble() < 0.5)
 				{
-					var session = zero.SelectLeft().Send(counter).Goto();
 					Console.WriteLine($"Push {counter}");
+					var s = session.SelectLeft().Send(counter);
 					counter++;
-					if (random.NextDouble() < 0.4)
-					{
-						var s1 = session.SelectLeft().Send(counter);
-						Console.WriteLine($"Push {counter}");
-						counter++;
-						zero = s1.Call((session, thisFunc) =>
-						{
-							if (random.NextDouble() < 0.4)
-							{
-								var s2 = session.SelectLeft().Send(counter);
-								Console.WriteLine($"Push {counter}");
-								counter++;
-
-								var s3 = s2.Call(thisFunc).Goto();
-								if (random.NextDouble() < 0.4)
-								{
-									var s4 = s3.SelectLeft().Send(counter).Goto();
-									Console.WriteLine($"Push {counter}");
-									counter++;
-									return thisFunc(s4, thisFunc);
-								}
-								else
-								{
-									return s3.SelectRight();
-								}
-							}
-							else
-							{
-								var s2 = session.SelectRight().Receive(out var value).Goto();
-								Console.WriteLine($"Pop {value}");
-								if (random.NextDouble() < 0.4)
-								{
-									var s3 = s2.SelectLeft().Send(counter).Goto();
-									Console.WriteLine($"Push {counter}");
-									counter++;
-									return thisFunc(s3, thisFunc);
-								}
-								else
-								{
-									return s2.SelectRight();
-								}
-							}
-						}).Goto();
-					}
-					else
-					{
-						zero = session.SelectRight().Receive(out var value).Goto();
-						Console.WriteLine($"Pop {value}");
-					}
+					var s2 = s.Call(thisFunc).Receive(out var x).Goto();
+					Console.WriteLine($"Pop {x}");
+					return thisFunc(s2, thisFunc);
 				}
 				else
 				{
-					zero.SelectRight().Close();
-					break;
+					return session.SelectRight();
 				}
-			};
+			}).Close();
 		}
 
 		/*
+		Hello World!
 		Push 0
 		Pop 0
 		Push 1
+		Pop 1
 		Push 2
-		Pop 2
 		Push 3
-		Pop 3
 		Push 4
-		Pop 4
 		Push 5
 		Push 6
+		Pop 6
 		Push 7
 		Push 8
+		Pop 8
+		Pop 7
+		Pop 5
 		Push 9
+		Pop 9
 		Push 10
 		Push 11
 		Pop 11
+		Pop 10
+		Pop 4
 		Push 12
-		Push 13
-		Push 14
-		Pop 14
-		Push 15
-		Pop 15
-		Push 16
-		Pop 16
-		Push 17
-		Pop 17
-		Push 18
-		Pop 18
-		Push 19
-		Pop 19
-		Push 20
-		Pop 20
-		Push 21
-		Pop 21
-		Push 22
-		Push 23
-		Pop 23
-		Push 24
-		Push 25
-		Pop 25
-		Push 26
-		Pop 26
-		Push 27
-		Push 28
-		Push 29
-		Pop 29
-		Push 30
-		Pop 30
-		Push 31
-		Push 32
-		Pop 32
-		Push 33
-		Pop 33
-		Push 34
-		Pop 34
-		Push 35
-		Push 36
-		Push 37
-		Pop 37
-		Push 38
-		Pop 38
-		Push 39
-		Pop 39
-		Push 40
-		Push 41
-		Pop 41
-		Push 42
-		Pop 42
-		Push 43
-		Pop 43
-		Push 44
-		Push 45
-		Pop 45
-		Push 46
-		Pop 46
-		Push 47
-		Push 48
-		Push 49
-		Push 50
-		Pop 50
-		Push 51
-		Push 52
-		Pop 52
-		Push 53
-		Pop 53
+		Pop 12
+		Pop 3
+		Pop 2
 		*/
 	}
 }
