c08e266441fbc1e7d4a06657f96e913ce7097d21
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-27 21:04:54 +0900

    衝突回避、3分岐

--- SessionTypes/SessionTypes/SessionTypes.cs
+++ SessionTypes/SessionTypes/SessionTypes.cs
@@ -10,9 +10,19 @@ namespace SessionTypes
 		private protected SessionType() { }
 	}
 
+	public sealed class Send<S> : SessionType where S : SessionType
+	{
+		private Send() { }
+	}
+
 	public sealed class Send<T, S> : SessionType where S : SessionType
 	{
 		private Send() { }
+	}
+
+	public sealed class Receive<S> : SessionType where S : SessionType
+	{
+		private Receive() { }
 	}
 
 	public sealed class Receive<T, S> : SessionType where S : SessionType
@@ -25,24 +35,24 @@ namespace SessionTypes
 		private Select() { }
 	}
 
+	public sealed class Select<L, C, R> : SessionType where L : SessionType where C : SessionType where R : SessionType
+	{
+		private Select() { }
+	}
+
 	public sealed class Follow<L, R> : SessionType where L : SessionType where R : SessionType
 	{
 		private Follow() { }
 	}
 
-	public sealed class Goto0 : SessionType
+	public sealed class Follow<L, C, R> : SessionType where L : SessionType where C : SessionType where R : SessionType
 	{
-		private Goto0() { }
+		private Follow() { }
 	}
 
-	public sealed class Goto1 : SessionType
+	public sealed class Call0 : SessionType
 	{
-		private Goto1() { }
-	}
-
-	public sealed class Goto2 : SessionType
-	{
-		private Goto2() { }
+		private Call0() { }
 	}
 
 	public sealed class Call0<S> : SessionType where S : SessionType
@@ -50,9 +60,19 @@ namespace SessionTypes
 		private Call0() { }
 	}
 
+	public sealed class Call1 : SessionType
+	{
+		private Call1() { }
+	}
+
 	public sealed class Call1<S> : SessionType where S : SessionType
 	{
 		private Call1() { }
+	}
+
+	public sealed class Call2 : SessionType
+	{
+		private Call2() { }
 	}
 
 	public sealed class Call2<S> : SessionType where S : SessionType
@@ -60,9 +80,29 @@ namespace SessionTypes
 		private Call2() { }
 	}
 
-	public sealed class Close : SessionType
+	public sealed class Eps : SessionType
 	{
-		private Close() { }
+		private Eps() { }
+	}
+
+	public abstract class SessionStack
+	{
+		private protected SessionStack() { }
+	}
+
+	public sealed class Push<F, E> : SessionStack where F : SessionType where E : SessionStack
+	{
+		private Push() { }
+	}
+
+	public sealed class Any : SessionStack
+	{
+		private Any() { }
+	}
+
+	public sealed class Empty : SessionStack
+	{
+		private Empty() { }
 	}
 
 	public abstract class SessionList : ProtocolType
@@ -78,21 +118,5 @@ namespace SessionTypes
 	public sealed class Nil : SessionList
 	{
 		private Nil() { }
-	}
-
-	public abstract class SessionStack
-	{
-		private protected SessionStack() { }
-	}
-
-	public sealed class Push<F, E> : SessionStack where F : SessionType where E : SessionStack
-	{
-		private Push() { }
-
-	}
-
-	public sealed class Empty : SessionStack
-	{
-		private Empty() { }
 	}
 }
