0d464d34eb582a97f81b1fa3976f8ea59b08d9e9
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-27 20:04:36 +0900

    Update ICommunicator

--- SessionTypes/SessionTypes/ICommunicator.cs
+++ SessionTypes/SessionTypes/ICommunicator.cs
@@ -4,30 +4,40 @@ namespace SessionTypes
 {
 	internal interface ICommunicator
 	{
+		public void Send();
+
 		public void Send<T>(T value);
+
+		public Task SendAsync();
 
 		public Task SendAsync<T>(T value);
 
+		public void Receive();
+
 		public T Receive<T>();
+
+		public Task ReceiveAsync();
 
 		public Task<T> ReceiveAsync<T>();
 
-		public void Select(Direction direction);
+		public void Select(Selection direction);
 
-		public Task SelectAsync(Direction direction);
+		public Task SelectAsync(Selection direction);
 
-		public Direction Follow();
+		public Selection Follow();
 
-		public Task<Direction> FollowAsync();
+		public Task<Selection> FollowAsync();
 
-		public Session<P, P> Cast<P, O>() where P : ProtocolType where O : ProtocolType;
+		public Session<S, Empty, P> CastNewChannel<S, P, O, Q>() where S : SessionType where P : ProtocolType where O : SessionType where Q : ProtocolType;
 
-		public Task<Session<P, P>> CastAsync<P, O>() where P : ProtocolType where O : ProtocolType;
+		public Task<Session<S, Empty, P>> CastNewChannelAsync<S, P, O, Q>() where S : SessionType where P : ProtocolType where O : SessionType where Q : ProtocolType;
 
-		public Session<P, P> Accept<P>() where P : ProtocolType;
+		public Session<S, Empty, P> AcceptNewChannel<S, P>() where S : SessionType where P : ProtocolType;
 
-		public Task<Session<P, P>> AcceptAsync<P>() where P : ProtocolType;
+		public Task<Session<S, Empty, P>> AcceptNewChannelAsync<S, P>() where S : SessionType where P : ProtocolType;
 
 		public void Close();
+
+		public void Die();
 	}
 }
