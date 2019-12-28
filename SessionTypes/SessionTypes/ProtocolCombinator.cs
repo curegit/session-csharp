3434bd97b18ad90b1951652e541dbf7c62084510
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-28 14:59:28 +0900

    ファイルを統合

--- SessionTypes/SessionTypes/ProtocolCombinator.cs
+++ SessionTypes/SessionTypes/ProtocolCombinator.cs
@@ -2,6 +2,36 @@ using System;
 
 namespace SessionTypes
 {
+	public sealed class UnitPayload
+	{
+		internal UnitPayload() { }
+	}
+
+	public sealed class Payload<T>
+	{
+		internal Payload() { }
+	}
+
+	public sealed class SerialPayloads<T1, T2>
+	{
+		internal SerialPayloads() { }
+	}
+
+	public sealed class SerialPayloads<T1, T2, T3>
+	{
+		internal SerialPayloads() { }
+	}
+
+	public sealed class SerialPayloads<T1, T2, T3, T4>
+	{
+		internal SerialPayloads() { }
+	}
+
+	public sealed class SerialPayloads<T1, T2, T3, T4, T5>
+	{
+		internal SerialPayloads() { }
+	}
+
 	public delegate Payload<T> PayloadDelegate<T>();
 
 	public delegate SerialPayloads<T1, T2> SerialPayloadDelegate<T1, T2>();
