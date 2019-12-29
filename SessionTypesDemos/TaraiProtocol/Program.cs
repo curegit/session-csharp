e9434defebd221856bc68019c521e96fbc169f68
Cure Git <37978051+curegit@users.noreply.github.com>
2019-12-29 18:01:15 +0900

    たらいを更新

--- SessionTypesDemos/TaraiProtocol/Program.cs
+++ SessionTypesDemos/TaraiProtocol/Program.cs
@@ -3,61 +3,44 @@ using System.Threading.Tasks;
 using SessionTypes;
 using SessionTypes.Threading;
 
-using System.Runtime.Serialization.Formatters.Binary;
-
 namespace TaraiProtocol
 {
-	//using static Combinator<>;
-	using static Portobuf;
 	using static ProtocolCombinator;
-	using static ProtocolCombinator<Protobuf>;
-	//using static ProtocolCom
-
-	namespace TaraiProtocol
-	{
-		public class TaraiProtocol
-		{
-
-		}
-
-		public class TaraiProtocol2 : TaraiProtocol
-		{
-
-		}
-	}
-
 
 	public class Program
 	{
 		public static async Task Main(string[] args)
 		{
-			var protocol = C2S(P<int>, C2S(P<int>, C2S(P<int>, S2CAdd(C2S(P<bool>, End), AtS(S2C(P<int>, End), End)))));
+			var protocol = Send(Value<int>, Send(Value<int>, Send(Value<int>, AcceptNewChannel(Send(Unit, End), Follow(Receive(Value<int>, End), End)))));
 
-			//protocol.
-
-			var client = protocol.Fork(async server =>
+			var client = protocol.ForkThread(async server =>
 			{
-				var s1 = server.Receive(out var x).Receive(out var y).Receive(out var z);
-				var s2 = s1.SendNewChannel(out var session);
-				var calc = Task.Run(() => Tarai(x, y, z));
-				var cancel = session.ReceiveAsync();
-				int i = Task.WaitAny(calc, cancel);
-				if (i == 0)
+				static int Tarai(int x, int y, int z, Task waitForCancel)
 				{
-					s2.SelectLeft().Send(calc.Result).Close();
-					(await cancel).Bind(out var b).Close();
+					if (waitForCancel.IsCompleted) throw new OperationCanceledException();
+					if (x <= y) return y;
+					return Tarai(Tarai(x - 1, y, z, waitForCancel), Tarai(y - 1, z, x, waitForCancel), Tarai(z - 1, x, y, waitForCancel), waitForCancel);
 				}
-				else
+
+				var s1 = server.Receive(out var x).Receive(out var y).Receive(out var z);
+				var s2 = s1.CastNewChannel(out var channelForCancel);
+
+				var waitForCancel = channelForCancel.ReceiveAsync();
+				try
+				{
+					var result = Tarai(x, y, z, waitForCancel);
+					s2.SelectLeft().Send(result).Close();
+					(await waitForCancel).Close();
+				}
+				catch
 				{
 					s2.SelectRight().Close();
-					cancel.Result.Bind(out var b).Close();
+					(await waitForCancel).Close();
 				}
-
-				
 			});
 
 			var c1 = client.Send(16).Send(3).Send(2);
-			var c2 = c1.ReceiveNewChannel(out var ch);
+			var c2 = c1.AcceptNewChannel(out var ch);
 			var ret = c2.FollowAsync(left =>
 			{
 				left.Receive(out var ans).Close();
@@ -68,22 +51,17 @@ namespace TaraiProtocol
 				right.Close();
 				Console.WriteLine("Canceled");
 			});
-			var timeout = Task.Delay(20000);
+			var timeout = Task.Delay(11500);
 			var task = await Task.WhenAny(ret, timeout);
 			if (task == ret)
 			{
-				ch.Send(false).Close();
+				ch.Send().Close();
 			}
 			else
 			{
-				ch.Send(true).Close();
+				ch.Send().Close();
 				await ret;
 			}
-		}
-
-		private static int Tarai(int x, int y, int z)
-		{
-			return x <= y ? y : Tarai(Tarai(x - 1, y, z), Tarai(y - 1, z, x), Tarai(z - 1, x, y));
 		}
 	}
 }
