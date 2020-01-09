a30b26ea5ab2294f72c303d04d29a55b81fe9bf1
Cure Git <37978051+curegit@users.noreply.github.com>
2020-01-09 22:29:08 +0900

    Disposer -> Canceller

---
+++ SessionCSharp/Session/SessionCanceller.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace Session
+{
+	public sealed class SessionCanceller : IDisposable
+	{
+		private bool open = true;
+
+		private readonly List<Session> sessions = new List<Session>();
+
+		public void Register(Session session)
+		{
+			if (open)
+			{
+				sessions.Add(session);
+			}
+			else
+			{
+				throw new ObjectDisposedException(nameof(SessionCanceller));
+			}
+		}
+
+		public void Dispose()
+		{
+			if (open)
+			{
+				open = false;
+				var exceptions = new List<Exception>();
+				foreach (var session in sessions)
+				{
+					try
+					{
+						session.Cancel();
+					}
+					catch (Exception exception)
+					{
+						exceptions.Add(exception);
+					}
+				}
+				if (exceptions.Count > 0)
+				{
+					throw new AggregateException(exceptions);
+				}
+			}
+		}
+	}
+}
