86205edd1ba0837f259799b6e8d850701ff6ddc5
Cure Git <37978051+curegit@users.noreply.github.com>
2020-01-13 14:46:47 +0900

    Update Tarai

--- SessionCSharpExamples/TaraiProtocol/Program.cs
+++ SessionCSharpExamples/TaraiProtocol/Program.cs
@@ -11,36 +11,38 @@ namespace TaraiProtocol
 	{
 		public static async Task Main(string[] args)
 		{
-			var protocol = Send(Value<int>, Send(Value<int>, Send(Value<int>, ReceiveNewChannel(Send(Unit, End), Follow(Receive(Value<int>, End), End)))));
+			var protocol = Send(Value<int>, Send(Value<int>, Send(Value<int>, CatchNewChannel(Send(Unit, End), Follow(Receive(Value<int>, End), End)))));
 
-			var client = protocol.ForkThread(async server =>
+			var client = protocol.ForkThread(async ch1 =>
 			{
-				static int Tarai(int x, int y, int z, Task waitForCancel)
+				static int Tarai(int x, int y, int z, Func<bool> cancelled)
 				{
-					if (waitForCancel.IsCompleted) throw new OperationCanceledException();
+					if (cancelled()) throw new OperationCanceledException();
 					if (x <= y) return y;
-					return Tarai(Tarai(x - 1, y, z, waitForCancel), Tarai(y - 1, z, x, waitForCancel), Tarai(z - 1, x, y, waitForCancel), waitForCancel);
+					return Tarai(Tarai(x - 1, y, z, cancelled), Tarai(y - 1, z, x, cancelled), Tarai(z - 1, x, y, cancelled), cancelled);
 				}
 
-				var s1 = server.Receive(out var x).Receive(out var y).Receive(out var z);
-				var s2 = s1.SendNewChannel(out var channelForCancel);
+				var ch2 = ch1.Receive(out var x).Receive(out var y).Receive(out var z);
+				var ch3 = ch2.ThrowNewChannel(out var channelForCancel);
 
 				var waitForCancel = channelForCancel.ReceiveAsync();
 				try
 				{
-					var result = Tarai(x, y, z, waitForCancel);
-					s2.SelectLeft().Send(result).Close();
-					(await waitForCancel).Close();
+					var result = Tarai(x, y, z, () => waitForCancel.IsCompleted);
+					ch3.SelectLeft().Send(result).Close();
 				}
 				catch
 				{
-					s2.SelectRight().Close();
+					ch3.SelectRight().Close();
+				}
+				finally
+				{
 					(await waitForCancel).Close();
 				}
 			});
 
 			var c1 = client.Send(16).Send(3).Send(2);
-			var c2 = c1.ReceiveNewChannel(out var ch);
+			var c2 = c1.CatchNewChannel(out var ch);
 			var ret = c2.FollowAsync(left =>
 			{
 				left.Receive(out var ans).Close();
