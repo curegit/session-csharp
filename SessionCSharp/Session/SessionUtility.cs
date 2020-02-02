8a37da03edf0c98f791390fbaa2b4d397c7252d3
Cure Git <37978051+curegit@users.noreply.github.com>
2020-02-02 14:45:21 +0900

    Update Utility

--- SessionCSharp/Session/SessionUtility.cs
+++ SessionCSharp/Session/SessionUtility.cs
@@ -30,8 +30,7 @@ namespace Session
 			return session.Duplicate();
 		}
 
-		/*
-		public static void Do<S, P>(this IEnumerable<Session<S, P>> sessions, Action<Session<S, P>> action) where S : ProtocolType where P : ProtocolType
+		public static void ForEach<S, P>(this IEnumerable<S> sessions, Action<S> action) where S : Session
 		{
 			foreach (var session in sessions)
 			{
@@ -39,34 +38,72 @@ namespace Session
 			}
 		}
 
-		
-		public static IReadOnlyList<Session<N, P>> Do<S, N, P>(this IEnumerable<Session<S, P>> sessions, Func<Session<S, P>, Session<N, P>> action)
-			where S : ProtocolType where N : ProtocolType where P : ProtocolType
+		public static IEnumerable<T> Map<S, T>(this IEnumerable<S> sessions, Func<S, T> func) where S : Session
 		{
-			var next = new List<Session<N, P>>();
-			foreach (var session in sessions)
-			{
-				action(session);
-			}
-			return next;
+			if (sessions is null) throw new ArgumentNullException(nameof(sessions));
+			if (func is null) throw new ArgumentNullException(nameof(func));
+			var results = new List<T>();
+			foreach (var session in sessions) results.Add(func(session));
+			return results;
 		}
 
-		// TODO: async Task version
-		*/
-		// TODO: Task
-		public static (IEnumerable<Session<SN, E, P>> s, IEnumerable<Session<S, E, P>> rs, IEnumerable<T> rargs) ZipWith<S, E, P, SN, T>(this IEnumerable<Session<S, E, P>> sessions, IEnumerable<T> args, Func<Session<S, E, P>, T, Session<SN, E, P>> f) where S : SessionType where E : SessionStack where P : ProtocolType where SN : SessionType
+		public static (IEnumerable<U> ziped, IEnumerable<S> sessRest, IEnumerable<T> argRest) ZipWith<S, T, U>(this IEnumerable<S> sessions, IEnumerable<T> args, Func<S, T, U> func) where S : Session
 		{
-			var a = new List<Session<SN, E, P>>();
-			var b = new List<Session<S, E, P>>();
-			List<T> c = new List<T>();
-			//var i = 0;
-			var t = sessions.GetEnumerator();
-			foreach (var h in args)
+			var ss = new List<S>();
+			var ts = new List<T>();
+			var us = new List<U>();
+
+			var se = sessions.GetEnumerator();
+			var ae = args.GetEnumerator();
+
+			while (true)
 			{
-				var d = f(t.Current, h);
-				t.MoveNext();
+				if (se.MoveNext())
+				{
+					if (ae.MoveNext())
+					{
+						us.Add(func(se.Current, ae.Current));
+						continue;
+					}
+					else
+					{
+						while (se.MoveNext())
+						{
+							ss.Add(se.Current);
+						}
+						break;
+					}
+				}
+				else
+				{
+					if (ae.MoveNext())
+					{
+						while (ae.MoveNext())
+						{
+							ts.Add(ae.Current);
+						}
+						break;
+					}
+					else
+					{
+						break;
+					}
+				}
 			}
-			return (a, b, c);
+			return (us, ss, ts);
+		}
+
+		public static (IEnumerable<T1>, IEnumerable<T2>) Unzip<T1, T2>(this IEnumerable<(T1, T2)> ss)
+		{
+			if (ss is null) throw new ArgumentNullException(nameof(ss));
+			var results1 = new List<T1>();
+			var results2 = new List<T2>();
+			foreach (var s in ss)
+			{
+				results1.Add(s.Item1);
+				results2.Add(s.Item2);
+			}
+			return (results1, results2);
 		}
 	}
 }
